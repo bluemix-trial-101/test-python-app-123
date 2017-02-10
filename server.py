8fbf6f89d64a795150294a7c722ca96697f9ab43
bluemix trial <bluemix.trial.101@gmail.com>
2017-02-09 23:59:54 -0600

    django based app

--- server.py
+++ server.py
@@ -1,21 +1,92 @@
+'''
+Run this with `$ python ./mini_django.py runserver` and go
+to http://localhost:8000/
+'''
 import os
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+import sys
+from django.conf import settings
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+# this module
+me = os.path.splitext(os.path.split(__file__)[1])[0]
+# helper function to locate this dir
+here = lambda x: os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
 
+# SETTINGS
+DEBUG = True
+ROOT_URLCONF = me
+DATABASES = {'default': {}}  # required regardless of actual usage
+TEMPLATE_DIRS = (
+    here('.'),  # Templates in current dir
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': TEMPLATE_DIRS,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': (
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
+            ),
+        },
+    },
+]
+SECRET_KEY = 'so so secret'
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+)
+INSTALLED_APPS = (
+    'django.contrib.sessions',
+)
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    here('static'),
+)
+
+SILENCED_SYSTEM_CHECKS = ['1_8.W001']  # Silance warning for using TEMPLATE_*
+
+if not settings.configured:
+    settings.configure(**locals())
+
+from django.shortcuts import render
+# Settings must be configured before importing some things
+# from django.views.decorators.csrf import csrf_exempt
+
+
+# VIEW
+def index(request, name=None):
+    return render(request, 'index.html', {'name': name})
+
+
+# URLS
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^(?P<name>\w+)?$', index),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if __name__ == '__main__':
+    # set the ENV
+    sys.path += (here('.'),)
+    # run the development server
+    from django.core import management
+    management.execute_from_command_line()
